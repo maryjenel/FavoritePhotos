9ec6305919a9648a38f87823831d521abc8db118
mewpre <yichin.sun@gmail.com>
2015-01-14 13:04:34 -0800

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Yi-Chin Sun on 1/14/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
